922441127725ee32f1829604171db31555a329bb
Stanislas Quastana <squastana@gmail.com>
2024-06-03 19:59:51 +0200

    ajout code

--- README.md
+++ README.md
@@ -6,14 +6,55 @@ This repo contains terraform code to deploy a simple network landing zone for Nu
 ## Prerequisites
 
 - All prerequisites for NC2 : https://portal.nutanix.com/page/documents/details?targetId=Nutanix-Clusters-AWS:aws-clusters-aws-requirements-c.html 
+- More information about NC2 on AWS : https://portal.nutanix.com/page/documents/details?targetId=Nutanix-Clusters-AWS:aws-clusters-aws-getting-started-c.html 
 
 - An AWS Account with enough privileges (create VPC, ...)
-- AWS CLI x or >: 
+- AWS CLI 2.15 or >: https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html or https://github.com/aws/aws-cli/tree/v2  
 - Terraform CLI 1.5 or > : <https://www.terraform.io/downloads.html>
 
 You can also clone this repo in your AWS Cloud Shell (that has all tools installed)
 
+
 ## Step by step operations
 
-Edit [configuration.tfvars](configuration.tfvars) to define your Azure resources names.
+Edit [configuration.tfvars](configuration.tfvars) to define your AWS resources names or tags, your AWS region...
+
+You can list your AWS region available using the following command :
+
+```bash
+aws ec2 describe-regions --output table
+```
+
+The following command gives the region actually used by the CLI regardless of whether environment variables are or are not set:
+
+```bash
+aws ec2 describe-availability-zones --output table --query 'AvailabilityZones[0].[RegionName]'
+```
+
+
+If you want to define your own IP ranges, edit [main.tf](main.tf)  (I will change that later to put everything as a variable)
+ 
+
+1. Terraform Init phase  
+
+```bash
+terraform init
+```
+
+2. Terraform Plan phase
+
+```bash
+terraform plan --var-file=configuration.tfvars
+```
+
+3. Terraform deployment phase (add TF_LOG=info at the beginning of the following command line if you want to see what's happen during deployment)
+
+```bash
+terraform apply --var-file=configuration.tfvars
+```
+
+4. Wait until the end of deployment (It should take less than 1 minute)
+
+
+
 
--- main.tf
+++ main.tf
@@ -12,42 +12,7 @@
 #     UVM subnets: /16 and /25, including both
 #         UVM subnet sizing would depend on the number of UVMs that would need to be deployed. 
 #         NC2 supports the network CIDR sizing limits enforced by AWS
-
-
-# AWS VPC
-# cf. https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest
-# cf https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest
-
-# module "vpc" {
-#   source = "terraform-aws-modules/vpc/aws"
-#   version = "5.8.1"
-
-#   name = "NC2-vpc"
-#   cidr = "10.0.0.0/16"
-#
-# # eu-west-3	= Paris Region
-#
-#   # azs             = ["eu-west-3a", "eu-west-3b", "eu-west-3c"]
-#   # private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-#   # public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-#
-#   azs             = ["eu-west-3a"]
-#   private_subnets = ["10.0.1.0/24"]
-#   public_subnets  = ["10.0.2.128/28"]
-#
-#   enable_nat_gateway = true
-#   enable_vpn_gateway = false
-#
-#   tags = {
-#     Terraform = "true"
-#     Environment = "demo"
-#   }
-# }
-
-
-
-
-
+# Please don't overlap AWS CIDR with your on-premises network CIDR
 
 
 
@@ -56,8 +21,13 @@
 
 resource "aws_vpc" "Terra-VPC" {
 
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = "10.0.0.0/16"   # CIDR requirements: /16 and /25 including both
   instance_tenancy = "default"
+  # if you want to use internal proxy for your NC2 cluster, you need to enable DNS hostnames
+  # and DNS support. In any case, NC2 documentation recommends to enable these settings.
+  # cf. https://docs.aws.amazon.com/vpc/latest/userguide/vpc-dns.html#vpc-dns-support
+  enable_dns_hostnames = true
+  enable_dns_support = true
 
   tags = {
     Name = join("", [var.VPC_NAME,"-",var.AWS_REGION])
@@ -70,7 +40,7 @@ resource "aws_vpc" "Terra-VPC" {
 
 resource "aws_subnet" "Terra-Public-Subnet" {
   vpc_id                  = aws_vpc.Terra-VPC.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.1.0/24"   # CIDR requirements: /16 and /25 including both
   availability_zone       = "eu-west-3a"
   map_public_ip_on_launch = true
 
@@ -80,14 +50,13 @@ resource "aws_subnet" "Terra-Public-Subnet" {
 }
 
 
-# A private subnet for management traffic
+# A private subnet for cluster management traffic
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet 
 
 resource "aws_subnet" "Terra-Private-Subnet-Mngt" {
   vpc_id                  = aws_vpc.Terra-VPC.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.2.0/24"    # CIDR requirements: /16 and /25 including both
   availability_zone       = "eu-west-3a"
-  map_public_ip_on_launch = true
 
   tags = {
     Name = join("", ["NC2-PrivateMgntSubnet-",var.AWS_REGION,"a"])
@@ -95,18 +64,35 @@ resource "aws_subnet" "Terra-Private-Subnet-Mngt" {
 }
 
 
-# One or more private subnets for user VM traffic
+# One or more private subnets for User VM (UVM) traffic
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet  
 
 resource "aws_subnet" "Terra-Private-Subnet-UVM1" {
   vpc_id                  = aws_vpc.Terra-VPC.id
-  cidr_block              = "10.0.3.0/24"
+  cidr_block              = "10.0.3.0/24"   # CIDR requirements: /16 and /25 including both
   availability_zone       = "eu-west-3a"
-  map_public_ip_on_launch = true
 
   tags = {
     ## join function https://developer.hashicorp.com/terraform/language/functions/join
     Name = join("", ["NC2-PrivateSubnet-UVM1-",var.AWS_REGION,"a"])
+  }
+}
+
+
+# One or more private subnets for Prism Central VM and MST
+# Subnets used for Prism Central and Multicloud Snapshot Technology (MST) must be different
+# than the UVM subnet
+# cf. https://portal.nutanix.com/page/documents/details?targetId=Nutanix-Clusters-AWS:aws-cluster-protect-requirements-c.html
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet  
+
+resource "aws_subnet" "Terra-Private-Subnet-PC" {
+  vpc_id                  = aws_vpc.Terra-VPC.id
+  cidr_block              = "10.0.4.0/24"   # CIDR requirements: /16 and /25 including both
+  availability_zone       = "eu-west-3a"
+
+  tags = {
+    ## join function https://developer.hashicorp.com/terraform/language/functions/join
+    Name = join("", ["NC2-PrivateSubnet-PC-",var.AWS_REGION,"a"])
   }
 }
 
@@ -120,7 +106,8 @@ resource "aws_internet_gateway" "Terra-Internet-Gateway" {
   }
 }
 
-# Elastic IP resource (EIP)
+
+# Elastic IP resource (EIP) - mandatory for AWS NAT Gateway
 # cf. https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip
 resource "aws_eip" "Terra-EIP" {
   domain   = "vpc"
@@ -149,6 +136,7 @@ resource "aws_nat_gateway" "Terra-AWS-NAT-GW" {
 
 }
 
+
 # Route Table for Public Subnet
 # cf. https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
 
@@ -160,40 +148,35 @@ resource "aws_route_table" "Terra-Public-Route-Table" {
     gateway_id = aws_internet_gateway.Terra-Internet-Gateway.id
   }
 
-  route {
-     # since this is exactly the route AWS will create, the route will be adopted
-    cidr_block = "10.0.1.0/24"
-    gateway_id = "local" # local route
-  }
-
   tags = {
     Name = "NC2-Route-Table-Public"
   }
 }
 
 
+# Route Table Association for Public Subnet
+# cf. https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association
+resource "aws_route_table_association" "Terra-Public-Route-Table-Association" {
+  subnet_id      = aws_subnet.Terra-Public-Subnet.id
+  route_table_id = aws_route_table.Terra-Public-Route-Table.id
+}
+
 
 # Route Table for Private Subnet(s)
 # cf. https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
-# this is the route table for the private subnets, to go to on-premises network or Internet
+# this is the route table for the private subnets, to go to on-premises network or Internet 
+# (communication of cluster with NC2 portal)
 
 resource "aws_route_table" "Terra-Private-Route-Table" {
   vpc_id = aws_vpc.Terra-VPC.id
 
-  # route {
-  #   cidr_block = "0.0.0.0/24"
-  #   gateway_id = aws_internet_gateway.example.id
-  # }
-
   route {
-    cidr_block = "10.0.2.0/24"
-    gateway_id = "local" # local route
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.Terra-AWS-NAT-GW.id
   }
-
-  route {
-    cidr_block = "10.0.3.0/24"
-    gateway_id = "local" # local route
-  }
+  ##### IMPORTANT #####
+  # Insert here you internal routes to on-premises network or other networks
+  #
 
   tags = {
     Name = "NC2-Route-Table-Private"
@@ -201,25 +184,42 @@ resource "aws_route_table" "Terra-Private-Route-Table" {
 }
 
 
-# AWS Security Group for Public Subnet
+# Route Table Association for Private Subnet Management
+# cf. https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table_association
+resource "aws_route_table_association" "Terra-Private-Route-Table-Association-Mngt" {
+  subnet_id      = aws_subnet.Terra-Private-Subnet-Mngt.id
+  route_table_id = aws_route_table.Terra-Private-Route-Table.id
+}
 
 
+# Route Table Association for Private Subnet UVM1
+resource "aws_route_table_association" "Terra-Private-Route-Table-Association-UVM1" {
+  subnet_id      = aws_subnet.Terra-Private-Subnet-UVM1.id
+  route_table_id = aws_route_table.Terra-Private-Route-Table.id
+}
 
-# AWS Security Group for Private Subnets
 
-
-
-
+# Route Table Association for Private Subnet PC
+resource "aws_route_table_association" "Terra-Private-Route-Table-Association-PC" {
+  subnet_id      = aws_subnet.Terra-Private-Subnet-PC.id
+  route_table_id = aws_route_table.Terra-Private-Route-Table.id
+}
 
 
 
 ### If there is a Web proxy configured #################
+# https://portal.nutanix.com/page/documents/details?targetId=Nutanix-Clusters-AWS:aws-aws-clusters-vpc-endpoints-for-s3-c.html
+# AWS VPC private endpoints for S3 and EC2 services must be configured when using a proxy server 
+# to communicate with the NC2 console.
+# These endpoints can connect to AWS Services privately from your VPC without going through the
+# public Internet.
+
 
 # VPC Endpoint for S3
 # cf. https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_endpoint
-# resource "aws_vpc_endpoint" "s3" {
-#   vpc_id       = aws_vpc.main.id
-#   service_name = "com.amazonaws.us-west-2.s3"
+# resource "aws_vpc_endpoint" "Terra-VPC-Endpoint-S3" {
+#   vpc_id       = aws_vpc.Terra-VPC.id
+#   service_name = join("", ["com.amazonaws.",var.AWS_REGION,".s3"])    # ex: "com.amazonaws.us-west-2.s3"
 
 #   tags = {
 #     Environment = "test"
@@ -230,7 +230,7 @@ resource "aws_route_table" "Terra-Private-Route-Table" {
 # cf. https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc_endpoint
 # resource "aws_vpc_endpoint" "ec2" {
 #   vpc_id            = aws_vpc.main.id
-#   service_name      = "com.amazonaws.us-west-2.ec2"
+#   service_name      = join("", ["com.amazonaws.",var.AWS_REGION,".ec2"])   # ex: "com.amazonaws.us-west-2.ec2"
 #   vpc_endpoint_type = "Interface"
 #   security_group_ids = [
 #     aws_security_group.sg1.id,
@@ -242,59 +242,245 @@ resource "aws_route_table" "Terra-Private-Route-Table" {
 
 
 
-#### For Cluster Protect #####
+#### For NC2 Cluster Protect #####
 
 # Two new AWS S3 buckets with Nutanix IAM role if you want to use 
 # the Cluster Protect feature to protect Prism Central, UVM, and volume groups data.
 # S3-Bucket
+# https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+# cf. https://portal.nutanix.com/page/documents/details?targetId=Nutanix-Clusters-AWS:aws-cluster-protect-creating-s3-buckets-c.html
+# Note: NC2 creates an IAM role with the required permissions to access S3 buckets with the nutanix-clusters prefix.
+#       This IAM role is added to the CloudFormation template. 
 # cf. https://registry.terraform.io/modules/terraform-aws-modules/s3-bucket/aws/latest 
 
+# Bucket for Prism Central backups
+resource "aws_s3_bucket" "Terra-S3-Bucket-PC" {
+  bucket_prefix = "nutanix-clusters-mst-pc"
+
+  tags = {
+    Name        = "nutanix-clusters-PC"
+  }
+}
+
+# Ensure that public access to MST PC  S3 buckets is blocked by default
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_public_access_block
+resource "aws_s3_bucket_public_access_block" "Terra-S3-Public-Access-Block-PC" {
+  bucket = aws_s3_bucket.Terra-S3-Bucket-PC.id 
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+  restrict_public_buckets = true
+}
+
+# Controlling versioning on MST PC S3 bucket
+# cf. https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_versioning
+resource "aws_s3_bucket_versioning" "Terra-S3-Bucket-PC-Versioning" {
+  bucket = aws_s3_bucket.Terra-S3-Bucket-PC.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Set Object Lock to Enable and Default retention period as 31 days.
+# This provides WORM configuration for objects to create point-in-time snapshots of Prism Central configuration
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_object_lock_configuration
+# In the context of AWS S3 Object Lock, there are two modes for retention: `GOVERNANCE` and `COMPLIANCE`.
+# `GOVERNANCE` mode allows users with specific permissions to override the retention settings.
+# This means that while the objects are protected under normal circumstances, a user with the `s3:BypassGovernanceRetention` permission can change 
+# the retention settings or delete the object before the retention period expires. This mode is useful when you want to protect objects from being deleted
+# or altered during the retention period, but still need the ability to do so in exceptional circumstances.
+# In the context of AWS S3 Object Lock, `COMPLIANCE` mode ensures that a protected object cannot be overwritten or
+# deleted by any user, including the root user in your AWS account. Once an object is locked in `COMPLIANCE` mode,
+# its retention mode cannot be changed, and its retention period cannot be shortened. 
+# This mode is typically used for regulatory compliance, where data must be preserved and not altered for a fixed period of time.
+resource "aws_s3_bucket_object_lock_configuration" "Terra-S3-Bucket-PC-Object-Lock" {
+  bucket = aws_s3_bucket.Terra-S3-Bucket-PC.id
+
+  rule {
+    default_retention {
+      mode = "GOVERNANCE"   # Should be COMPLIANCE for regulatory compliance
+      days = 31
+    }
+  }
+}
+
+# Configure the Object Lifecycle rule to auto-delete older backup data, and the rule scope must be set to apply to all objects in the bucket.
+# Also, select the following Lifecycle rule actions:
+# - Expire current version of objects: set as 31 days
+# - Permanently delete noncurrent versions of objects: set Days after objects become noncurrent to 1 day.
+# - Delete expired object, delete markers, or incomplete multipart uploads: Number of days as 1 day.
+# cf. https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_lifecycle_configuration
+resource "aws_s3_bucket_lifecycle_configuration" "Terra-S3-Bucket-PC-Lifecycle" {
+  bucket = aws_s3_bucket.Terra-S3-Bucket-PC.id
+
+  rule {
+    id      = "AutoDeleteOlderBackupData"
+    status  = "Enabled"
+
+    expiration {
+      days = 31
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = 1
+    }
+
+    abort_incomplete_multipart_upload { 
+      days_after_initiation  = 1
+    }
+  }
+}
+
+
+
+# Bucket for UVM backups
+resource "aws_s3_bucket" "Terra-S3-Bucket-UVM" {
+  bucket_prefix = "nutanix-clusters-mst-uvm"
+
+  tags = {
+    Name        = "nutanix-clusters-UVM"
+  }
+}
+
+# Ensure that public access to MST UVM S3 buckets is blocked by default
+# cf. https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_public_access_block
+resource "aws_s3_bucket_public_access_block" "Terra-S3-Public-Access-Block-UVM" {
+  bucket = aws_s3_bucket.Terra-S3-Bucket-UVM.id 
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+  restrict_public_buckets = true
+}
+
+# Controlling versioning on MST UVM S3 bucket
+# cf. https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_versioning
+resource "aws_s3_bucket_versioning" "Terra-S3-Bucket-UVM-Versioning" {
+  bucket = aws_s3_bucket.Terra-S3-Bucket-UVM.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Set Object Lock to Enable and Default retention period as 31 days.
+# This provides WORM configuration for objects to create point-in-time snapshots of Prism Central configuration
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_object_lock_configuration
+resource "aws_s3_bucket_object_lock_configuration" "Terra-S3-Bucket-UVM-Object-Lock" {
+  bucket = aws_s3_bucket.Terra-S3-Bucket-UVM.id
+
+  rule {
+    default_retention {
+      mode = "GOVERNANCE"   # Should be COMPLIANCE for regulatory compliance
+      days = 31
+    }
+  }
+}
+
+# Configure the Object Lifecycle rule to auto-delete older backup data, and the rule scope must be set to apply to all objects in the bucket.
+# Also, select the following Lifecycle rule actions:
+# - Expire current version of objects: set as 31 days
+# - Permanently delete noncurrent versions of objects: set Days after objects become noncurrent to 1 day.
+# - Delete expired object, delete markers, or incomplete multipart uploads: Number of days as 1 day.
+# cf. https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_lifecycle_configuration
+resource "aws_s3_bucket_lifecycle_configuration" "Terra-S3-Bucket-UVM-Lifecycle" {
+  bucket = aws_s3_bucket.Terra-S3-Bucket-UVM.id
+
+  rule {
+    id      = "AutoDeleteOlderBackupData"
+    status  = "Enabled"
+
+    expiration {
+      days = 31
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = 1
+    }
+
+    abort_incomplete_multipart_upload { 
+      days_after_initiation  = 1
+    }
+  }
+}
 
 
 
 
+# AWS Security Group for VPC
+# cf. https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/default_security_group
+# resource "aws_default_security_group" "Terra-Security-Group-vpc" {
+#   vpc_id = aws_vpc.Terra-VPC.id
 
+#   ingress {
+#     protocol  = -1
+#     self      = true
+#     from_port = 0
+#     to_port   = 0
+#   }
 
-
-
-
-
-
-
-# module "app_security_group" {
-#   source  = "terraform-aws-modules/security-group/aws//modules/web"
-#   version = "4.17.0"
-
-#   name        = "web-sg-project-alpha-dev"
-#   description = "Security group for web-servers with HTTP ports open within VPC"
-#   vpc_id      = module.vpc.vpc_id
-
-#   ingress_cidr_blocks = module.vpc.public_subnets_cidr_blocks
-
-#   tags = {
-#     project     = "project-alpha",
-#     environment = "dev"
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
 #   }
 # }
 
-# resource "random_string" "lb_id" {
-#   length  = 3
-#   special = false
-# }
 
 
-# module "ec2_instances" {
-#   source = "./modules/aws-instance"
+# AWS Security Group for management interfaces	
+# cf. https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
 
-#   depends_on = [module.vpc]
-
-#   instance_count     = 2
-#   instance_type      = "t2.micro"
-#   subnet_ids         = module.vpc.private_subnets[*]
-#   security_group_ids = [module.app_security_group.security_group_id]
-
+# resource "aws_security_group" "Terra-Security-Group-Mngt" {
+#   name        = "NC2-SG-User-Management"
+#   description = "Security Group for management interfaces"
+#   vpc_id      = aws_vpc.Terra-VPC.id
 #   tags = {
-#     project     = "project-alpha",
-#     environment = "dev"
+#     Name = "NC2 User Management Security Group"
 #   }
 # }
+
+
+# AWS Security Group for Internal Management
+# cf. https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+
+# resource "aws_security_group" "Terra-Security-Group-Internal-Mngt" {
+#   name        = "NC2-SG-Internal-Mngt"
+#   description = "Security Group for Internal Management"
+#   vpc_id      = aws_vpc.Terra-VPC.id
+
+#   tags = {
+#     Name = "NC2 Internal Management Security Group"
+#   }
+# }
+
+
+# AWS Security Group for PC VMs 
+# cf. https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+
+# resource "aws_security_group" "Terra-Security-Group-PC" {
+#   name        = "NC2-SG-PC"
+#   description = "Security Group for PC VMs"
+#   vpc_id      = aws_vpc.Terra-VPC.id
+
+#   tags = {
+#     Name = "NC2 PC VMs Security Group"
+#   }
+# }
+
+
+# AWS Security Group for UVMs 
+# cf. https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+
+# resource "aws_security_group" "Terra-Security-Group-UVM1" {
+#   name        = "NC2-SG-UVM1"
+#   description = "Security Group for UVMs"
+#   vpc_id      = aws_vpc.Terra-VPC.id
+
+#   tags = {
+#     Name = "NC2 User VMs Security Group"
+#   }
+# }
+
+
